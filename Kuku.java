db14f867f9aacff6a7626b6bc67b96869ffab573
wada@tagbangers.co.jp <Munyomunyo.04>
2020-02-07 11:10:00 +0900

    九九が一覧表示できるようにした

--- Kuku.java
+++ Kuku.java
@@ -1,11 +1,26 @@
 public class Kuku{
   public static void main (String[] args){
+    System.out.print("    ");
+
     for(int i=1;i<10;i++){
-      System.out.println(i + "の段");
+      System.out.print(i);
+      System.out.print("  ");
+    }
+    System.out.println();
+
+    for(int i=1;i<10;i++){
+      System.out.print(i + "|");
       for(int j=1;j<10;j++){
-      System.out.println(i + "×" + j + "=" + i * j);
+        if(i*j<10){
+          System.out.print("  ");
+          System.out.print(i * j);
+        }
+        if(i*j>9){
+          System.out.print(" ");
+          System.out.print(i * j);
+        }
       }
-      System.out.println();
+      System.out.println("");
     }
   }
 }
